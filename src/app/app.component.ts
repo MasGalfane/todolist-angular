24e1b5087d43efae92e5b891db64c3c5407f00cf
Mouzdaher <mouzdaher.a@gmail.com>
2021-10-26 16:52:10 +0200

    stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Todo} from '../models/todo';
 import {AbstractControl, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -28,40 +29,80 @@ export class AppComponent implements OnInit {
   // new FormArray();
   public form = this.fb.array([]);
 
+  /**
+   * Inject Angular's FormBuilder in our component.
+   */
   public constructor(
     private fb: FormBuilder,
   ) {
   }
 
+  /**
+   * Call at component initialization. Use to prepare and set value in the form from todoList data source.
+   */
   public ngOnInit(): void {
     for (let i = 0; i < this.todoList.length; i++) {
       this.addTodo();
     }
 
-    this.form.setValue(this.todoList);
+    this.form.setValue(this.todoList.map((todo: Todo) => {
+      return {
+        label: todo.label,
+        at: formatDate(todo.at, 'YYYY-MM-dd', 'en'),
+        finished: todo.finished,
+      };
+    }));
+
+    // This code bellow do the same as above.
+    // const arrTmp = [];
+    // for (const todo of this.todoList) {
+    //   const formTodo = {
+    //     label: todo.label,
+    //     at: formatDate(todo.at, 'YYYY-MM-dd', 'en'),
+    //     finished: todo.finished,
+    //   };
+    //   arrTmp.push(formTodo);
+    // }
+    // this.form.setValue(arrTmp);
   }
 
+  /**
+   * Add a new form for To do in formArray.
+   */
   public addTodo(): void {
+    const d = new Date();
+    d.setDate(d.getDate() + 7);
+
     this.form.push(
       // new FormGroup();
       this.fb.group({
         // new FormControl();
         label: [''],
         // new FormControl();
-        at: [new Date()],
+        at: [formatDate(d, 'YYYY-MM-dd', 'en')],
         // new FormControl();
         finished: [false],
       })
     );
   }
 
+  public removeTodo(index: number): void{
+    this.form.removeAt(index);
+  }
+
+  /**
+   * Generic method to be use with all keys inside a FormGroup.
+   */
   public getControl(formGroup: AbstractControl, key: string): FormControl
   {
+    // We cannot set the type of the first argument as FormGroup (in template, we get AbstractControl).
+    // Check if the method's first argument is of type FormGroup.
     if (!(formGroup instanceof FormGroup)) {
       throw new Error('Form given as first argument is not an instance of FormGroup');
     }
 
     const fc = formGroup.get(key);
+    // Check if the field retrieve by given key is an instance of FormControl.
     if (!(fc instanceof FormControl)) {
       throw new Error('Form retrieve is not an instance of FormControl');
     }
@@ -69,8 +110,12 @@ export class AppComponent implements OnInit {
     return fc;
   }
 
+  // In our case, this function do the same result as the one use above. (But without strict type checking, because no instanceof).
   // public getControl(formGroup: AbstractControl, key: string): FormControl
   // {
   //   return (formGroup as FormGroup).get(key) as FormControl;
   // }
+
+
+
 }
